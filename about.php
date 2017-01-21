9bc175406068004446dadaba11a7730853e1b18d
Martin Wright <martin@mynameismartin.com>
2017-01-21 16:33:24 +0000

    remove Rdio from about page

--- about.php
+++ about.php
@@ -37,10 +37,7 @@
 		    	<li><a href="http://flickr.com/photos/skip694" class="flickr"><i class="ss-icon ">flickr</i></a></li>
 		    	<li><a href="http://www.last.fm/user/Johnny-5" class="lastfm"><i class="ss-icon">lastfm</i></a></li>
 		    	<li><a href="http://instagram.com/mynameismartin/" class="instagram"><i class="ss-icon ">instagram</i></a></li>
-		    	<li><a href="http://www.rdio.com/people/wrightmartin/" class="rdio"><i class="ss-icon ">rdio</i></a></li>
 		    	<li><a href="https://www.facebook.com/thenameismartin" class="facebook"><i class="ss-icon ">facebook</i></a></li>
-
-
 			</ul>
 		</section>
 
